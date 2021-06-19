7f67953d31c57255f595e8af479272c3e689ce6e
Amanb007 <62053657+Amanb007@users.noreply.github.com>
2021-06-19 13:48:55 +0530

    Add files via upload

---
+++ Stage 3/Lombok, Sonar/TraineeManager/src/main/java/com/cognizant/traineemanager/exceptions/InvalidTraineeDataException.java
@@ -0,0 +1,10 @@
+package com.cognizant.traineemanager.exceptions;
+
+public class InvalidTraineeDataException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+
+	public InvalidTraineeDataException(String message) {
+		super(message);
+	}
+}
